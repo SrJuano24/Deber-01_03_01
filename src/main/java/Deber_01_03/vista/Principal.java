29b8946d016cbacd827684b31431090534461aa5
Juano <Juano@DESKTOP-48VCEGK>
2022-11-17 18:30:48 -0500

    Terminado

--- src/main/java/Deber_01_03/vista/Principal.java
+++ src/main/java/Deber_01_03/vista/Principal.java
@@ -31,6 +31,22 @@ public class Principal {
         data[11] = "Calle Larga";
         data[12] = "09908723478";
         
+        var cliente2 =new String[13];
+        cliente2[0] = "01";
+        cliente2[1] = "AMAZON";
+        cliente2[2] = "0102453949001";
+        cliente2[3] = "Pio Bravo";
+        cliente2[4] = "4112349";
+        cliente2[5] = "20";
+        cliente2[6] = "25.8";
+        // Cliente
+        cliente2[7] = "02";
+        cliente2[8] = "02";
+        cliente2[9] = "Pedro Ramirez";
+        cliente2[10] = "0302456980";
+        cliente2[11] = "Calle Larga";
+        cliente2[12] = "09908723478";
+        
         var data1 = new String[13];
         data1[0] = "02";
         data1[1] = "AMAZON";
@@ -41,8 +57,8 @@ public class Principal {
         data1[6] = "25.8";
 
         // Cliente
-        data1[7] = "02";
-        data1[8] = "02";
+        data1[7] = "03";
+        data1[8] = "01";
         data1[9] = "Pedro Ramirez";
         data1[10] = "0302456980";
         data1[11] = "Calle Larga";
@@ -59,7 +75,7 @@ public class Principal {
 
         // Cliente
         data2[7] = "02";
-        data2[8] = "02";
+        data2[8] = "03";
         data2[9] = "Pedro Ramirez";
         data2[10] = "0302456980";
         data2[11] = "Calle Larga";
@@ -67,8 +83,10 @@ public class Principal {
         
         
         System.out.println(facturaControl.crear(data));
+         System.out.println(facturaControl.crear(cliente2));
         System.out.println(facturaControl.crear(data1));
         System.out.println(facturaControl.crear(data2));
+       
         System.out.println("|||||||||||||||||||||||||||||||");
         System.out.println(facturaControl.listar());
         System.out.println("-------------------------------");
